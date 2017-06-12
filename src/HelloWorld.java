4ece0fd0a6ad2afabcda6de56b9ac0ec3f74932b
Happy Home <ursankar@gmail.com>
2017-06-11 19:54:29 -0700

    First Hello World java Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * @author Happy Home
+ * @version 1.0
+ */
+public class HelloWorld {
+	
+	/**
+	 * @param args 
+	 * 
+	 */
+	public static void main(String[] args) {
+	
+		String msg = "hi";
+		// TODO Auto-generated method stub
+		System.out.println("My first java program "+msg);
+	}
+
+}
