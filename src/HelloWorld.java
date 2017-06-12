e126f376e6dd677e370fa8ff212a3adb78608626
Happy Home <ursankar@gmail.com>
2017-06-11 20:22:24 -0700

    updated code

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -14,7 +14,7 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 	
-		String msg = "hi THere";
+		String msg = "hi THere Friend";
 		// TODO Auto-generated method stub
 		System.out.println("My first java program "+msg);
 	}
